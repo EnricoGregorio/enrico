086927cd090dc854e77e7a5c8880ed68988875d8
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2021-10-05 10:46:03 -0300

    Avanço no js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,24 @@
-
-// xpTrabalhos.style.display = 'none'
-
-// function mostrarTrabalhos() {
-//     cursos.style.display = 'none'
-//     return xpTrabalhos.style.display = 'block'
-// }
-
-// function mostrarEducacao() {
-//     xpTrabalhos.style.display = 'none'
-//     return cursos.style.display = 'block'
-// }
-
 $(function () {
     let cursos = $('div.educacao')
     let xpTrabalhos = $('div.trabalhos')
     $("#job").click(function () {
         if (xpTrabalhos.is(':hidden') == true) {
-            xpTrabalhos.fadeIn(2000)
             cursos.fadeOut()
+            xpTrabalhos.fadeIn(1500)
             window.document.querySelector("#edu").style.backgroundColor = "#FFFFFF"
             window.document.querySelector("#job").style.backgroundColor = "#F2F2F2"
-            window.document.querySelector("#job").style.borderRadius = "10px"
         }
     })
     $("#edu").click(function () {
         if (cursos.is(':hidden') == true) {
-            cursos.fadeIn(2000)
             xpTrabalhos.fadeOut()
+            cursos.fadeIn(1500)
             window.document.querySelector("#job").style.backgroundColor = "#FFFFFF"
             window.document.querySelector("#edu").style.backgroundColor = "#F2F2F2"
-            window.document.querySelector("#edu").style.borderRadius = "10px"
         }
     })
+    
+    $("form").click(function() {
+        window.open("mailto:enricogregorio5@gmail.com")
+    })
 })
-
-// $("button").click(function() {
-//     $("div.educacao").fadeout("slow");
-//     $("div.trabalhos").fadein("slow");
-// });
-
-// $("div.trabalhos").click(function() {
-//     $("div.trabalhos").fadeout("slow");
-//     $("div.educacao").fadein("slow");
-// });
