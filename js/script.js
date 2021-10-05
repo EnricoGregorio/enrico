c4eb63ce9eac3ecf782d9b2e5db4d4f6017786f4
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2021-10-05 01:43:48 -0300

    Terminando site
    Terminei o leyout para o desktop, mas ainda falta começar e terminar o responsivo

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
-let cursos = window.document.querySelector('.educacao')
-let xpTrabalhos = window.document.querySelector('.trabalhos')
-xpTrabalhos.style.display = 'none'
 
-function mostrarTrabalhos() {
-    cursos.style.display = 'none'
-    return xpTrabalhos.style.display = 'block'
-}
+// xpTrabalhos.style.display = 'none'
 
-function mostrarEducacao() {
-    xpTrabalhos.style.display = 'none'
-    return cursos.style.display = 'block'
-}
+// function mostrarTrabalhos() {
+//     cursos.style.display = 'none'
+//     return xpTrabalhos.style.display = 'block'
+// }
 
-$("button").click(function() {
-    $(cursos).fadeout("slow");
-    $(xpTrabalhos).fadein("slow");
-});
+// function mostrarEducacao() {
+//     xpTrabalhos.style.display = 'none'
+//     return cursos.style.display = 'block'
+// }
 
-$("button").click(function() {
-    $(xpTrabalhos).fadeout("slow");
-    $(cursos).fadein("slow");
-});
+$(function () {
+    let cursos = $('div.educacao')
+    let xpTrabalhos = $('div.trabalhos')
+    $("#job").click(function () {
+        if (xpTrabalhos.is(':hidden') == true) {
+            xpTrabalhos.fadeIn(2000)
+            cursos.fadeOut()
+            window.document.querySelector("#edu").style.backgroundColor = "#FFFFFF"
+            window.document.querySelector("#job").style.backgroundColor = "#F2F2F2"
+            window.document.querySelector("#job").style.borderRadius = "10px"
+        }
+    })
+    $("#edu").click(function () {
+        if (cursos.is(':hidden') == true) {
+            cursos.fadeIn(2000)
+            xpTrabalhos.fadeOut()
+            window.document.querySelector("#job").style.backgroundColor = "#FFFFFF"
+            window.document.querySelector("#edu").style.backgroundColor = "#F2F2F2"
+            window.document.querySelector("#edu").style.borderRadius = "10px"
+        }
+    })
+})
+
+// $("button").click(function() {
+//     $("div.educacao").fadeout("slow");
+//     $("div.trabalhos").fadein("slow");
+// });
+
+// $("div.trabalhos").click(function() {
+//     $("div.trabalhos").fadeout("slow");
+//     $("div.educacao").fadein("slow");
+// });
