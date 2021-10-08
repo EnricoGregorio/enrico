1dac1736fb2a6f2c54e1125b09d8ee93fb2cfa72
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2021-10-08 11:43:46 -0300

    Navegação responsiva
    Adicionei a navegação responsiva com a função .slideToggle, do jquery, nas páginas Home e Contato

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 $(function () {
+
+    // Header
+    // Botão do header
+    
+    /* <i class="fas fa-times"></i> */
+
+    //Funções no header
+    $(".menu-button").click(function() {
+        let menuMobile = $('nav.mobile')
+        menuMobile.slideToggle()
+    })
+
+    // Sessão habilidades
+    // Botões da sessão habilidades
     let cursos = $('div.educacao')
     let xpTrabalhos = $('div.trabalhos')
-    $("#job").click(function () {
+
+    // Funções na sessão habilidades
+    $("#job").click(function() {
         if (xpTrabalhos.is(':hidden') == true) {
             cursos.fadeOut()
             xpTrabalhos.fadeIn(1500)
@@ -9,7 +25,8 @@ $(function () {
             window.document.querySelector("#job").style.backgroundColor = "#F2F2F2"
         }
     })
-    $("#edu").click(function () {
+
+    $("#edu").click(function() {
         if (cursos.is(':hidden') == true) {
             xpTrabalhos.fadeOut()
             cursos.fadeIn(1500)
