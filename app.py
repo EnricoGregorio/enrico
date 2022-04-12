7e27f05c7621568b40e33acd041de2b212fa45da
Enrico Sobrinho Gregório <enricogregorio5@gmail.com>
2022-04-11 22:59:22 -0300

    Add Python as Backend
    adicionei o Python com Flask como back end, onde atua nas rotas do index e de erro do site

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect
+app = Flask(__name__)
+
+redirectHome = redirect('http://127.0.0.1:5000/')
+
+@app.route('/', methods=['GET', 'POST'])
+def showHomePage():
+    pageHome = render_template('index.html')
+    return pageHome
+
+# Função para carregar uma página que não existe no site.
+@app.route('/<inexistente>', methods=['GET'])
+def showErrorPage(inexistente):
+    pageError = render_template('error404.html')
+    return pageError
